8bd5fa00b666ed86b62038ffa325fdf36a645103
Kyungguk Min <kyungguk@me.com>
2019-01-14 07:32:27 +0900

    hybrid_1d project created.

---
+++ Makefile
@@ -0,0 +1,11 @@
+export CXX=xcrun --sdk macosx clang++
+#export CXX=g++
+
+export CFLAGS=-O3 -std=c++17
+export LDFLAGS=-O3 -flto
+
+all:
+	@make -eC ./hybrid_1d/hybrid_1d all
+
+clean:
+	@make -eC ./hybrid_1d/hybrid_1d clean
---
+++ hybrid_1d/hybrid_1d/main.cc
@@ -0,0 +1,17 @@
+//
+//  main.cc
+//  hybrid_1d
+//
+//  Created by KYUNGGUK MIN on 1/14/19.
+//  Copyright © 2019 kyungguk.com. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    {
+        // insert code here...
+        std::cout << "Hello, World!" << std::endl;
+    }
+    return 0;
+}
