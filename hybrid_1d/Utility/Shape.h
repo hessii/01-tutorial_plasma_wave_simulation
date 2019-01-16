b13b5c4e3ee7dac31ef50b99531b8311fd48455b
Kyungguk Min <kyungguk@me.com>
2019-01-16 19:12:49 +0900

    Shape function pretty print.

--- hybrid_1d/Utility/Shape.h
+++ hybrid_1d/Utility/Shape.h
@@ -13,6 +13,7 @@
 #include "../Macros.h"
 
 #include <cmath>
+#include <ostream>
 
 HYBRID1D_BEGIN_NAMESPACE
 template <long Order> struct Shape;
@@ -23,16 +24,26 @@ template <>
 struct Shape<1> {
     Real w[2];
     long i[2];
-    
+
     explicit Shape() noexcept = default;
-    explicit Shape(Real x) noexcept : Shape() { (*this)(x); }
-    
+    explicit Shape(Real x) noexcept { (*this)(x); }
+
     void operator()(Real x) noexcept {
         // where x = x/Dx
         i[0] = i[1] = static_cast<long>(std::floor(x));
         i[1] += 1;
         w[1] = x - i[0];
         w[0] = 1 - w[1];
+    }
+
+    // pretty print
+    //
+    template <class CharT, class Traits>
+    friend std::basic_ostream<CharT, Traits> &operator<<(std::basic_ostream<CharT, Traits> &os, Shape<1> const &s) {
+        return os << "Shape("
+        << "indices = {" << s.i[0] << ", " << s.i[1] << "}, "
+        << "weights = {" << s.w[0] << ", " << s.w[1] << "}"
+        << ")";
     }
 };
 
@@ -42,14 +53,14 @@ template <>
 struct Shape<2> {
     long i[3];
     Real w[3];
-    
+
     explicit Shape() noexcept = default;
-    explicit Shape(Real x) noexcept : Shape() { (*this)(x); }
-    
+    explicit Shape(Real x) noexcept { (*this)(x); }
+
     void operator()(Real x) noexcept {
         // where x = x/Dx
         constexpr Real half = 0.5, _3_4 = 0.75;
-        
+
         i[0] = i[1] = i[2] = static_cast<long>(std::round(x));
         i[0] -= 1;
         i[2] += 1;
@@ -67,6 +78,16 @@ struct Shape<2> {
         // i = i2
         //
         w[2] = 1 - (w[0]+w[1]);
+    }
+
+    // pretty print
+    //
+    template <class CharT, class Traits>
+    friend std::basic_ostream<CharT, Traits> &operator<<(std::basic_ostream<CharT, Traits> &os, Shape<2> const &s) {
+        return os << "Shape("
+        << "indices = {" << s.i[0] << ", " << s.i[1] << ", " << s.i[2] << "}, "
+        << "weights = {" << s.w[0] << ", " << s.w[1] << ", " << s.w[2] << "}"
+        << ")";
     }
 };
 HYBRID1D_END_NAMESPACE
