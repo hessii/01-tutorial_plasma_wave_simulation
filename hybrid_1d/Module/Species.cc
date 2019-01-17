327ebd17e14bdc1639130d8955b3290fbc7614f9
Kyungguk Min <kyungguk@me.com>
2019-01-17 10:43:08 +0900

    H1D::Species class basic definition.

--- hybrid_1d/Module/Species.cc
+++ hybrid_1d/Module/Species.cc
@@ -10,3 +10,9 @@
 #include "./EField.h"
 #include "./BField.h"
 #include "../Utility/ParticlePush.h"
+#include "../Inputs.h"
+
+H1D::Species::Species(Real const Oc, Real const op, long const Nc)
+: Nc(Nc), Oc(Oc), op(op), bucket(), _mom() {
+    bucket.resize(static_cast<unsigned long>(Nc*Input::Nx));
+}
