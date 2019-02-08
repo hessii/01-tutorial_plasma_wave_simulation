d59cbc7ae01217796320063c6116d9ef97a24467
Kyungguk Min <kyungguk@me.com>
2019-02-08 14:35:03 +0900

    Edge case fix in println.h.

--- hybrid_1d/Utility/println.h
+++ hybrid_1d/Utility/println.h
@@ -10,14 +10,20 @@
 #define println_h
 
 #include <initializer_list>
+#include <type_traits>
 #include <utility>
 #include <ostream>
 
 // synthetic sugar for output stream operator (<<)
 //
 namespace {
+    template <class CharT, class Traits> // single argument case
+    std::basic_ostream<CharT, Traits> &print(std::basic_ostream<CharT, Traits> &os) {
+        return os;
+    }
     template <class CharT, class Traits, class... Args>
-    std::basic_ostream<CharT, Traits> &print(std::basic_ostream<CharT, Traits> &os, Args&&... args) {
+    typename std::enable_if<sizeof...(Args) != 0, std::basic_ostream<CharT, Traits> // guard against empty parameter pack
+    >::type &print(std::basic_ostream<CharT, Traits> &os, Args&&... args) {
         (void)std::initializer_list<int>{
             (os << std::forward<Args>(args), 0)...
         }; // a neat trick for parameter pack expansion
