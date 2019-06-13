c75386b709729f8674d7d94fbfe8bb82e3439574
Kyungguk Min <kyungguk@me.com>
2019-06-13 09:54:52 +0900

    H1D::MasterDelegate and H1D::WorkerDelegate implementation guide/template.

--- hybrid_1d/Module/WorkerDelegate.cc
+++ hybrid_1d/Module/WorkerDelegate.cc
@@ -7,3 +7,48 @@
 //
 
 #include "WorkerDelegate.h"
+#include "Charge.h"
+#include "Current.h"
+#include "Species.h"
+
+#include <stdexcept>
+
+void H1D::WorkerDelegate::gather(Domain const& domain, Charge &charge)
+{
+    // 1. local gather first
+    //
+    Delegate::gather(domain, charge);
+    throw std::domain_error(__PRETTY_FUNCTION__);
+
+    // 2. reduce to master
+    //
+
+    // 3. broadcast to workers
+    //
+}
+void H1D::WorkerDelegate::gather(Domain const& domain, Current &current)
+{
+    // 1. local gather first
+    //
+    Delegate::gather(domain, current);
+    throw std::domain_error(__PRETTY_FUNCTION__);
+
+    // 2. reduce to master
+    //
+
+    // 3. broadcast to workers
+    //
+}
+void H1D::WorkerDelegate::gather(Domain const& domain, Species &sp)
+{
+    // 1. local gather first
+    //
+    Delegate::gather(domain, sp);
+    throw std::domain_error(__PRETTY_FUNCTION__);
+
+    // 2. reduce to master
+    //
+
+    // 3. broadcast to workers
+    //
+}
