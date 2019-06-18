e99c8e17ae72ea0b939e36910317353472a0a8ab
Kyungguk Min <kyungguk@me.com>
2019-06-18 13:16:47 +0900

    More type safe experimental implementation of InterThreadComm.

--- hybrid_1d/Boundary/InterThreadComm.h
+++ hybrid_1d/Boundary/InterThreadComm.h
@@ -14,6 +14,113 @@
 #include <type_traits>
 #include <utility>
 #include <atomic>
+#include <tuple>
+
+HYBRID1D_BEGIN_NAMESPACE
+namespace experimental {
+/// one-way inter-thread communicator
+///
+template <class Tx, class Rx, class... Payloads>
+class InterThreadComm {
+    static_assert(sizeof...(Payloads) > 0, "invalid number of channels");
+    InterThreadComm(InterThreadComm const&) = delete;
+    InterThreadComm &operator=(InterThreadComm const&) = delete;
+
+public:
+    static constexpr unsigned number_of_channels() noexcept { return sizeof...(Payloads); }
+
+    // one-way communication coordinator
+    //
+    struct Coordinator {
+    private:
+        std::atomic_flag flag1{};
+        std::atomic_flag flag2{};
+        //
+        static void _notify(std::atomic_flag &flag) noexcept {
+            flag.clear(std::memory_order_release);
+        }
+        static void _wait_for(std::atomic_flag &flag) noexcept {
+            while (flag.test_and_set(std::memory_order_acquire));
+        }
+    public:
+        Coordinator() noexcept {
+            flag1.test_and_set();
+            flag2.test_and_set();
+        }
+        //
+        void notify_tx() noexcept { _notify(flag1); }
+        void notify_rx() noexcept { _notify(flag2); }
+        //
+        void wait_for_transmit() noexcept { _wait_for(flag2); }
+        void wait_for_receipt() noexcept { _wait_for(flag1); }
+    };
+private:
+    std::tuple<std::pair<Coordinator, Payloads>...> channels{};
+
+public:
+    InterThreadComm() noexcept {}
+
+public:
+    class Ticket {
+        friend InterThreadComm;
+        Coordinator *coord;
+        void (Coordinator::*done)(void) noexcept;
+        Ticket(Coordinator *coord, void (Coordinator::*done)(void) noexcept) noexcept : coord{coord}, done{done} {}
+    public:
+        Ticket() noexcept : Ticket{nullptr, nullptr} {}
+        Ticket(Ticket &&o) noexcept : Ticket{} { std::swap(std::tie(coord, done), std::tie(o.coord, o.done)); }
+        Ticket &operator=(Ticket &&o) noexcept { std::swap(std::tie(coord, done), std::tie(o.coord, o.done)); return *this; }
+        //
+        ~Ticket() { this->operator()(); }
+        void operator()() noexcept {
+            if (coord) {
+                (coord->*done)();
+                coord = nullptr;
+                done = nullptr;
+            }
+        }
+    };
+
+    // tx thread
+    //
+    template <long i, class Payload> [[nodiscard]]
+    Ticket send([[maybe_unused]] Tx const& tx_tag, [[maybe_unused]] std::integral_constant<long, i> ch_tag, Payload&& payload)
+    {
+        auto &[coord, pkt] = std::get<i>(channels);
+
+        // 1. send data
+        //
+        pkt = std::forward<Payload>(payload);
+        coord.notify_rx();
+
+        // 2. return ticket
+        //
+        return {&coord, &Coordinator::wait_for_receipt()};
+    }
+
+    // rx thread
+    //
+    template <long i> [[nodiscard]]
+    auto recv([[maybe_unused]] Rx const& rx_tag, [[maybe_unused]] std::integral_constant<long, i> ch_tag)
+    -> std::pair<Ticket, std::tuple_element_t<i, std::tuple<Payloads...>>>
+    {
+        auto &[coord, pkt] = std::get<i>(channels);
+
+        // 1. wait for data
+        //
+        coord.wait_for_transmit();
+
+        // 2. return packet
+        //
+        return {{&coord, &Coordinator::notify_tx}, std::move(pkt)};
+    }
+};
+}
+HYBRID1D_END_NAMESPACE
+
+#include <type_traits>
+#include <utility>
+#include <atomic>
 #include <array>
 #include <any>
 
