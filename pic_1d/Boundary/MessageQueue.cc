6369088d8e47d1d2fabfe5707456652d48b3589e
Kyungguk Min <kyungguk@me.com>
2020-01-03 17:29:13 +0900

    MessageQueue implemented and test.

--- pic_1d/Boundary/MessageQueue.cc
+++ pic_1d/Boundary/MessageQueue.cc
@@ -7,3 +7,61 @@
 //
 
 #include "MessageQueue.h"
+#include "../Utility/println.h"
+
+#include <iostream>
+#include <memory>
+#include <chrono>
+#include <array>
+
+using P1D::MessageQueue;
+
+namespace {
+    void test_1() {
+        println(std::cout, "in ", __PRETTY_FUNCTION__);
+
+        MessageQueue<long, std::unique_ptr<std::string>> q;
+        auto tk1 = q.send<0>({0, 1}, long{1});
+        auto tk2 = q.send({0, 1}, std::make_unique<std::string>(__PRETTY_FUNCTION__));
+        long const i = q.recv<long>({0, 1});
+        auto const s = **q.recv<1>({0, 1});
+        tk1.get();
+        tk2.get();
+        println(std::cout, i, ", ", s);
+    }
+    void test_2() {
+        println(std::cout, "in ", __PRETTY_FUNCTION__);
+
+        constexpr int N = 10;
+        MessageQueue<std::unique_ptr<long>, long> q;
+        auto f = [&q](int const i) -> long {
+            using namespace std::chrono_literals;
+            std::this_thread::sleep_for(1s);
+            q.send({i + 1, 0}, std::make_unique<long>(i + 1)).get();
+            std::this_thread::sleep_for(1s);
+            return q.recv<long>({0, i + 1});
+        };
+
+        std::array<std::future<long>, N> flist;
+        for (unsigned i = 0; i < N; ++i) {
+            flist[i] = std::async(std::launch::async, f, i);
+        }
+
+        long sum = 0;
+        for (int i = 0; i < N; ++i) {
+            sum += **q.recv<0>({i + 1, 0});
+        }
+        for (int i = 0; i < N; ++i) {
+            q.send({0, i + 1}, static_cast<long const>(sum)).get();
+        }
+
+        for (auto &f : flist) {
+            println(std::cout, f.get());
+        }
+    }
+}
+
+void P1D::test_message_queue() {
+    test_1();
+    test_2();
+}
--- pic_1d/Boundary/MessageQueue.h
+++ pic_1d/Boundary/MessageQueue.h
@@ -22,73 +22,135 @@
 #include <queue>
 
 PIC1D_BEGIN_NAMESPACE
-/// MPI-like message queue
+/// MPI-like message (payload) passing machinary
 ///
 template <class... Payloads>
 class MessageQueue {
+    static_assert((... && std::is_move_constructible_v<Payloads>), "Payloads should be move-constructible");
+public:
+    MessageQueue() = default;
     MessageQueue(MessageQueue const&) = delete;
     MessageQueue &operator=(MessageQueue const&) = delete;
 
-public:
+public: // helpers
     template <class Payload>
     class Queue;
+    using Ticket = std::future<void>;
+
+    // payload wrapper ensuring delivery notification
     //
     template <class Payload>
     class Package {
         Payload payload;
-        std::promise<void> p{};
+        std::promise<void> promise{};
     private:
         friend Queue<Payload>;
-        Package(Payload&& payload) noexcept(std::is_nothrow_move_constructible_v<Payload>)
-        : payload{std::move(payload)} {}
     public:
-        ~Package() { p.set_value(); }
-        [[nodiscard]] operator Payload() && noexcept(std::is_nothrow_move_constructible_v<Payload>) {
-            return std::move(this->payload);
+        Package(Package&&) noexcept(std::is_nothrow_move_constructible_v<Payload>) = default;
+        Package& operator=(Package&&) noexcept(std::is_nothrow_move_assignable_v<Payload>) = default;
+        explicit Package(Payload&& payload) noexcept(std::is_nothrow_move_constructible_v<Payload>)
+        : payload{std::move(payload)} {}
+        //
+        [[nodiscard]] operator Payload() && {
+            Payload payload = std::move(this->payload);
+            promise.set_value(); // notify of delivery
+            return payload;
+        }
+        [[nodiscard]] Payload operator*() && {
+            return std::move(*this);
         }
     };
+
+    //per-payload-type message queue
     //
     template <class Payload>
     class Queue {
-        std::unordered_map<long, std::queue<Package<Payload>>> q{};
+        std::unordered_map<long, std::queue<Package<Payload>>> map{};
         std::atomic_flag flag = ATOMIC_FLAG_INIT;
     private:
         template <class F, class... Args>
         auto sync(F&& f, Args&&... args) & noexcept(std::is_nothrow_invocable_v<F&&, Args&&...>) {
             static_assert(std::is_invocable_v<F&&, Args&&...>);
+            // acquire access
             do {} while (flag.test_and_set(std::memory_order_acquire));
-            std::invoke_result_t<F&&, Args&&...> res = std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
+            // do work
+            std::invoke_result_t<F&&, Args&&...> res =
+            std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
+            // relinquish access
             flag.clear(std::memory_order_release);
+            // return, if any, result
             return res;
         }
     public:
-        [[nodiscard]] std::future<void> enqueue(long const key, Payload payload) {
-            return sync([&q = this->q](long const key, Payload payload) {
-                q[key].emplace(std::move(payload)).p.get_future();
-            }, key, std::move(payload));
+        [[nodiscard]] Ticket operator()(long const key, Payload&& payload) & {
+            return map[key].emplace(std::move(payload)).promise.get_future();
         }
-        [[nodiscard]] Package<Payload> dequeue(long const key) {
-            using Opt = std::optional<Package<Payload>>;
-            auto f = [&map = this->q](long const key) -> Opt {
-                if (map.count(key)) {
-                    if (auto &q = map[key]; !q.empty()) {
-                        Opt payload{std::move(q.front())};
-                        q.pop();
-                        return payload;
-                    }
+        [[nodiscard]] Ticket enqueue(long const key, Payload&& payload) & {
+            return sync(*this, key, std::move(payload));
+        }
+        //
+        [[nodiscard]] std::optional<Package<Payload>> operator()(long const key) & {
+            // only if the entry is present
+            if (map.count(key)) {
+                // only if at least one item in the queue
+                if (auto &q = map[key]; !q.empty()) {
+                    auto payload = std::move(q.front());
+                    q.pop();
+                    return std::move(payload);
                 }
-                return std::nullopt;
-            };
-            //
-            auto&& opt = sync(f, key);
+            }
+            return std::nullopt;
+        }
+        [[nodiscard]] Package<Payload> dequeue(long const key) & {
+            auto opt = sync(*this, key);
             while (!opt) {
                 std::this_thread::yield();
-                opt = sync(f, key);
+                opt = sync(*this, key);
             }
             return *std::move(opt);
         }
     };
+
+    //
+    //
+    union [[nodiscard]] Envelope {
+    private:
+        std::pair<int, int> pair;
+        long id;
+    public:
+        constexpr Envelope(int addresser, int addressee) noexcept : pair{addresser, addressee} {}
+        constexpr operator long() const noexcept { return id; }
+    };
+
+private:
+    std::tuple<Queue<Payloads>...> pool{};
+
+public: // sender
+    template <long I, class Payload> [[nodiscard]]
+    Ticket send(Envelope const envelope, Payload&& payload) {
+        return std::get<I>(pool).enqueue(envelope, std::move(payload));
+    }
+    template <class Payload> [[nodiscard]]
+    Ticket send(Envelope const envelope, Payload&& payload) {
+        using T = Queue<std::remove_reference_t<Payload>>;
+        return std::get<T>(pool).enqueue(envelope, std::move(payload));
+    }
+
+public: // receiver
+    template <long I> [[nodiscard]]
+    auto recv(Envelope const envelope) {
+        return std::get<I>(pool).dequeue(envelope);
+    }
+    template <class Payload> [[nodiscard]]
+    auto recv(Envelope const envelope) {
+        using T = Queue<std::remove_reference_t<Payload>>;
+        return std::get<T>(pool).dequeue(envelope);
+    }
 };
+
+// not for public use
+//
+void test_message_queue();
 PIC1D_END_NAMESPACE
 
 #endif /* MessageQueue_h */
