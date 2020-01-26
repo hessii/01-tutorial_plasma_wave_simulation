aec1e4cf6b513b65c4817a8c1b01f3c53afe8d5f
Kyungguk Min <kyungguk@me.com>
2020-01-27 07:06:00 +0900

    Snapshot.{h,cc} added.

---
+++ pic_1d/Recorder/Snapshot.cc
@@ -0,0 +1,9 @@
+//
+//  Snapshot.cc
+//  pic_1d
+//
+//  Created by KYUNGGUK MIN on 1/27/20.
+//  Copyright © 2020 Kyungguk Min & Kaijun Liu. All rights reserved.
+//
+
+#include "Snapshot.h"
---
+++ pic_1d/Recorder/Snapshot.h
@@ -0,0 +1,14 @@
+//
+//  Snapshot.h
+//  pic_1d
+//
+//  Created by KYUNGGUK MIN on 1/27/20.
+//  Copyright © 2020 Kyungguk Min & Kaijun Liu. All rights reserved.
+//
+
+#ifndef Snapshot_h
+#define Snapshot_h
+
+#include <stdio.h>
+
+#endif /* Snapshot_h */
