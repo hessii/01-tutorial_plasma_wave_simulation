1a86f2bf36ec1fd88a5f6872bb24608436ffd2fb
Kyungguk Min <kyungguk@me.com>
2020-01-27 08:07:17 +0900

    Snapshot basic interface.

--- pic_1d/Recorder/Snapshot.cc
+++ pic_1d/Recorder/Snapshot.cc
@@ -7,3 +7,17 @@
 //
 
 #include "Snapshot.h"
+
+P1D::Snapshot::message_dispatch_t P1D::Snapshot::dispatch;
+P1D::Snapshot::Snapshot(unsigned const rank, unsigned const size, ParamSet const &params, long const step_count)
+: comm{dispatch.comm(rank)}, size{size}, step_count{step_count}
+{
+    // method dispatch
+    //
+    save = is_master() ? &Snapshot::save_master : &Snapshot::save_worker;
+    load = is_master() ? &Snapshot::load_master : &Snapshot::load_worker;
+
+    // calculate signature
+    //
+    (void)params;
+}
--- pic_1d/Recorder/Snapshot.h
+++ pic_1d/Recorder/Snapshot.h
@@ -9,6 +9,46 @@
 #ifndef Snapshot_h
 #define Snapshot_h
 
-#include <stdio.h>
+#include "../Utility/MessageDispatch.h"
+#include "../Module/Domain.h"
+
+#include <vector>
+
+PIC1D_BEGIN_NAMESPACE
+class Snapshot {
+    void (Snapshot::*save)(Domain const &domain) &;
+    long (Snapshot::*load)(Domain &domain) const &;
+    long const step_count;
+    long signature{};
+
+public:
+    using PartBucket = PartSpecies::bucket_type;
+    using message_dispatch_t = MessageDispatch<std::vector<Scalar>, std::vector<Vector>, std::vector<Tensor>, PartBucket>;
+    using interthread_comm_t = message_dispatch_t::Communicator;
+
+    static message_dispatch_t dispatch;
+    interthread_comm_t const comm;
+    unsigned const size;
+    static constexpr unsigned master = 0;
+    bool is_master() const noexcept { return master == comm.rank(); }
+
+public:
+    explicit Snapshot(unsigned const rank, unsigned const size, ParamSet const &params, long const step_count);
+
+private: // load/save
+    void save_master(Domain const &domain) &;
+    void save_worker(Domain const &domain) &;
+    long load_master(Domain &domain) const &;
+    long load_worker(Domain &domain) const &;
+
+private: // load/save interface
+    friend void operator<<(Snapshot &snapshot, Domain const &domain) {
+        return (snapshot.*snapshot.save)(domain);
+    }
+    friend long operator>>(Snapshot const &snapshot, Domain &domain) {
+        return (snapshot.*snapshot.load)(domain);
+    }
+};
+PIC1D_END_NAMESPACE
 
 #endif /* Snapshot_h */
