9dbcac16473295bc83da1d68d7c733e10de4956c
Kyungguk Min <kyungguk@me.com>
2020-02-22 05:53:38 +0900

    05_waves_in_hot_isotropic_plasmas initial import.

---
+++ examples/05_waves_in_hot_isotropic_plasmas/Inputs.h
@@ -0,0 +1,133 @@
+//
+//  Inputs.h
+//  pic_1d
+//
+//  Created by KYUNGGUK MIN on 1/14/19.
+//  Copyright © 2019 Kyungguk Min & Kaijun Liu. All rights reserved.
+//
+
+#ifndef Inputs_h
+#define Inputs_h
+
+/// simulation input parameters;
+/// modify the variables under the `Input' namespace
+/// consult "Predefined.h" header for symbol definitions and constants
+///
+struct Input {
+    //
+    // MARK:- Housekeeping
+    //
+
+    /// number of worker threads to spawn for parallelization
+    ///
+    /// value `0' means serial update; value `n' means parallelization using n + 1 threads
+    /// part_desc.Nc*Nx must be divisible by n + 1, and
+    /// n + 1 must be divisible by number_of_subdomains
+    ///
+    static constexpr unsigned number_of_worker_threads = 19;
+
+    /// number of subdomains for domain decomposition (positive integer)
+    ///
+    /// Nx must be divisible by this number
+    ///
+    static constexpr unsigned number_of_subdomains = 1;
+
+    /// flag to suppress transverse electromagnetic fields
+    ///
+    static constexpr bool is_electrostatic = true;
+
+    //
+    // MARK: Global parameters
+    //
+
+    /// light speed
+    ///
+    static constexpr Real c = 1;
+
+    /// magnitude of uniform background magnetic field
+    ///
+    static constexpr Real O0 = 1;
+
+    /// angle in degrees between the x-axis and the uniform magnetic field direction.
+    ///
+    static constexpr Real theta = 0;
+
+    /// simulation grid size
+    ///
+    static constexpr Real Dx = M_PI/10*2;
+
+    /// number of grid points
+    ///
+    static constexpr unsigned Nx = 480;
+
+    /// time step size
+    ///
+    static constexpr Real dt = 0.01*2;
+
+    /// number of time steps for inner loop
+    /// total time step Nt = inner_Nt * outer_Nt
+    /// simulation time t = dt*Nt
+    ///
+    static constexpr unsigned inner_Nt = 10;
+
+    /// number of time steps for outer loop
+    /// total time step Nt = inner_Nt * outer_Nt
+    /// simulation time t = dt*Nt
+    ///
+    static constexpr unsigned outer_Nt = 5000;
+
+    //
+    // MARK: Plasma Species Descriptions
+    //
+
+    /// kinetic plasma descriptors
+    ///
+    static constexpr auto part_descs =
+    std::make_tuple(BiMaxPlasmaDesc({{-O0, c*0.0707107}, 2500, _2nd, full_f}, .005, 1, 3),
+                    BiMaxPlasmaDesc({{-O0, c*0.0707107}, 2500, _2nd, full_f}, .005, 1, -3)
+                    );
+
+    /// cold fluid plasma descriptors
+    ///
+    static constexpr auto cold_descs =
+    std::make_tuple(ColdPlasmaDesc({-O0, c*0.997497}));
+
+    //
+    // MARK: Data Recording
+    //
+
+    /// a top-level directory to which outputs will be saved
+    ///
+    static constexpr char working_directory[] = "./data";
+
+    /// field and particle energy density recording frequency; in units of inner_Nt
+    /// `0' means `not interested'
+    ///
+    static constexpr unsigned energy_recording_frequency = 1;
+
+    /// electric and magnetic field recording frequency
+    ///
+    static constexpr unsigned field_recording_frequency = 10;
+
+    /// species moment recording frequency
+    ///
+    static constexpr unsigned moment_recording_frequency = 0;
+
+    /// simulation particle recording frequency
+    ///
+    static constexpr unsigned particle_recording_frequency = 500;
+
+    /// maximum number of particles to dump
+    ///
+    static constexpr std::array<unsigned,
+    std::tuple_size_v<decltype(part_descs)>> Ndumps = {100000};
+};
+
+/// debugging options
+///
+namespace Debug {
+    constexpr bool zero_out_electromagnetic_field = false;
+    constexpr Real initial_efield_noise_amplitude = 0e0;
+}
+
+#endif /* Inputs_h */
---
+++ examples/05_waves_in_hot_isotropic_plasmas/README.md
@@ -0,0 +1,64 @@
+# Overview
+
+This tutorial illustrates waves in cold plasmas.
+The goal is to identify different modes of propagation depending on the propagation angle.
+
+
+# Build and Run
+
+This tutorial includes three demo simulations located in `01_parallel`, `02_perpendicular`,
+and `03_intermediate`, respectively.
+
+As usual, visit each directory, build the project using `make all`, and execute `./pic_1d`.
+
+The demos produce `field-*.csv` files.
+
+
+# Demo Parameters
+
+The demos assume the same parameters as the homework problems.
+That is, ions are immobile and electrons are cold (magnetoionic plasma).
+The uniform magnetostatic field is in the *z* direction, and the wave vector is assumed to lie in
+the *x*-*z* plane.
+The ratio of the electron plasma frequency to the electron cyclotron frequency is *ωpe*/*Ωce* = 2.
+
+Time is normalized to 1/*Ωce* and the distance to the electron skin depth, *de*.
+
+
+# Dispersion Relations
+
+We are going to use the three components of the electric field and display them in frequency
+and wave number space by taking Fast Fourier Transform.
+The simulated dispersion relations are compared with those from magnetoionic theory (homeworks).
+
+
+- At θ = 0 
+
+The figure below displays power spectral densities of the fluctuating electric field propagating
+parallel to the background magnetic field.
+The white dashed curves represent the theoretical dispersion relations for (from top to bottom)
+the RCP, LCP, and RCP modes, respectively.
+
+![Dispersion Relation at Parallel Propagation](./figures/01_parallel-efield_dispersion.png)
+
+
+- At θ = 90°
+
+The figure below displays power spectral densities of the fluctuating electric field propagating
+perpendicular to the background magnetic field.
+The white dashed curves represent the theoretical dispersion relations for
+two extraordinary modes (abscent in *Ez*) and the ordinary mode (appearling only in *Ez*).
+
+![Dispersion Relation at Perpendicular Propagation](./figures/02_perpendicular-efield_dispersion.png)
+
+
+- At θ = 60°
+
+The figure below displays power spectral densities of the fluctuating electric field propagating
+at an intermediate angle from the background magnetic field.
+The white dashed curves represent the theoretical dispersion relations for
+the extraordinary modes and the ordinary mode.
+The black dashed curves represent the theoretical dispersion relations for
+the RCP and LCP modes.
+
+![Dispersion Relation at Intermediate Propagation](./figures/03_intermediate-efield_dispersion.png)
