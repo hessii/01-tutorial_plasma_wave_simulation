c75270dae00b6fd9fb8e88a49292901e0c8cc7d7
Kyungguk Min <kyungguk@me.com>
2020-05-02 22:31:26 +0900

    VHistogramRecorder.{h,cc} added.

---
+++ pic_1d/Recorder/VHistogramRecorder.cc
@@ -0,0 +1,9 @@
+//
+//  VHistogramRecorder.c
+//  pic_1d
+//
+//  Created by KYUNGGUK MIN on 5/2/20.
+//  Copyright © 2020 Kyungguk Min & Kaijun Liu. All rights reserved.
+//
+
+#include "VHistogramRecorder.h"
---
+++ pic_1d/Recorder/VHistogramRecorder.h
@@ -0,0 +1,14 @@
+//
+//  VHistogramRecorder.h
+//  pic_1d
+//
+//  Created by KYUNGGUK MIN on 5/2/20.
+//  Copyright © 2020 Kyungguk Min & Kaijun Liu. All rights reserved.
+//
+
+#ifndef VHistogramRecorder_h
+#define VHistogramRecorder_h
+
+#include <stdio.h>
+
+#endif /* VHistogramRecorder_h */
