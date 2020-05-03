9bb44f8aa21110db961661d9c6e4ccbef20fa961
Kyungguk Min <kyungguk@me.com>
2020-05-04 07:40:08 +0900

    In Range, arithmatic operators implemented.

--- pic_1d/Utility/Range.h
+++ pic_1d/Utility/Range.h
@@ -30,6 +30,68 @@ struct [[nodiscard]] Range {
         return x >= min() && x < max();
     }
 
+    // compound operations
+    //
+    constexpr Range &operator+=(Real const &o) noexcept {
+        loc += o;
+        return *this;
+    }
+    constexpr Range &operator-=(Real const &o) noexcept {
+        return *this += -o;
+    }
+    constexpr Range &operator*=(Real const &o) noexcept {
+        if (o >= 0) {
+            loc *= o; len *= o;
+        } else {
+            loc = o*max(); len *= -o;
+        }
+        return *this;
+    }
+    constexpr Range &operator/=(Real const &o) noexcept {
+        return *this *= 1/o;
+    }
+
+    // unary operations
+    //
+    [[nodiscard]] friend constexpr Range const &operator+(Range const &s) noexcept {
+        return s;
+    }
+    [[nodiscard]] friend constexpr Range operator-(Range s) noexcept {
+        s *= -1;
+        return s;
+    }
+
+    // binary operations
+    //
+    [[nodiscard]] friend constexpr Range operator+(Range a, Real const &b) noexcept {
+        a += b;
+        return a;
+    }
+    [[nodiscard]] friend constexpr Range operator-(Range a, Real const &b) noexcept {
+        a -= b;
+        return a;
+    }
+    [[nodiscard]] friend constexpr Range operator*(Range a, Real const &b) noexcept {
+        a *= b;
+        return a;
+    }
+    [[nodiscard]] friend constexpr Range operator/(Range a, Real const &b) noexcept {
+        a /= b;
+        return a;
+    }
+
+    [[nodiscard]] friend constexpr Range operator+(Real const &b, Range a) noexcept {
+        a += b;
+        return a;
+    }
+    [[nodiscard]] friend constexpr Range operator-(Real const &b, Range const &a) noexcept {
+        return (-a) + b;
+    }
+    [[nodiscard]] friend constexpr Range operator*(Real const &b, Range a) noexcept {
+        a *= b;
+        return a;
+    }
+
 private:
     // pretty print
     //
