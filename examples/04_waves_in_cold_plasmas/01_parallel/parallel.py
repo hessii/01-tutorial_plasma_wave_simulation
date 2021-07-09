61c5eb237cee9519e5857b95842cd4f6a843a4c5
Hyeonmin Lee <dlgusals905@gmail.com>
2021-07-09 17:46:36 +0900

    parallel.py

---
+++ examples/04_waves_in_cold_plasmas/01_parallel/parallel.py
@@ -0,0 +1,14 @@
+# Waves in cold plasmas
+# 01_parallel
+# propagation angle theta = 0 deg
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import glob
+
+# importing data
+
+
+
+
