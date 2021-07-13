d267e3df7b10c973bc7073150d439fd12b5a26e0
Hyeonmin Lee <dlgusals905@gmail.com>
2021-07-13 20:40:00 +0900

    parallel E_x done

--- examples/04_waves_in_cold_plasmas/01_parallel/parallel.py
+++ examples/04_waves_in_cold_plasmas/01_parallel/parallel.py
@@ -5,12 +5,14 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+from scipy import signal
+import seaborn as sns
 import glob
 from natsort import natsorted
 
 Dx = 1.5e-1
 Nx = 960
+Dt = 1.5e-1
 
 # import all field files
 field = glob.glob('field*.csv')
@@ -29,4 +31,28 @@ def ext(index):
 dEx = ext(' dE2')
 
 # 2D FFT
+fft = np.fft.fft2(dEx, norm='forward')
+fft = np.fft.fftshift(fft)
+freq1 = np.fft.fftshift(np.fft.fftfreq(fft.shape[0], Dt)) * (2*np.pi)
+freq2 = np.fft.fftshift(np.fft.fftfreq(fft.shape[1], Dx)) * (2*np.pi)
+dk = freq2[1] - freq2[0]
+dw = freq1[1] - freq1[0]
+psd = np.abs(fft)**2 / (dk*dw)      # power spectral density
+i = freq2 > 0
 
+# plot
+fig1 = plt.figure(figsize=(10, 15))
+plt.contourf(freq2, freq1, np.log10(psd))
+plt.xlim(0, 5)
+plt.ylim(0, 10)
+
+plt.colorbar()
+plt.xticks(np.arange(6), ['', '1', '2', '3', '4', '5'], fontsize=24)
+plt.yticks(fontsize=24)
+plt.xlabel(r'$kd_e$', fontsize=28)
+plt.ylabel(r'$\omega/\Omega_{ce}$', fontsize=28)
+plt.title(r'$E_{1,x}$', fontsize=28, position=(0.9, 0))
+
+plt.savefig('./parallel_Ex.pdf')
+
+plt.show()
