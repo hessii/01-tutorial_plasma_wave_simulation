4c1cd1d273cfa6ad376bb63ff253536a6878e6b8
Hyeonmin Lee <dlgusals905@gmail.com>
2021-07-20 20:36:08 +0900

    electron dynamics

---
+++ examples/05_waves_in_hot_isotropic_plasmas/03_two_stream_instability/electron_dynamics.py
@@ -0,0 +1,17 @@
+# Waves in hot isotropic plasmas - Two-Stream Instability
+# Electron dynamics
+#
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import glob
+from natsort import natsorted
+
+
+
+
+
+
+
