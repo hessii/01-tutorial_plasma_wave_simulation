9a0649d80e6dfaca9ff39670e0c6319cf9865f40
Hyeonmin Lee <dlgusals905@gmail.com>
2021-07-24 16:49:22 +0900

    histogram fixed. final

--- examples/05_waves_in_hot_isotropic_plasmas/03_two_stream_instability/electron_dynamics.py
+++ examples/05_waves_in_hot_isotropic_plasmas/03_two_stream_instability/electron_dynamics.py
@@ -24,30 +24,26 @@ for i in np.arange(len(f2)):
     f = pd.read_csv(f2[i], 'r', skiprows=1, delimiter=',')
     v_sp2.append(f['v1'])
 
-#
-#v0 =
+# read vx at t = 400, 800, 1200, 1600, 2000
 v400 = v_sp1[1].append(v_sp2[1])
 v800 = v_sp1[3].append(v_sp2[3])
 v1200 = v_sp1[5].append(v_sp2[5])
 v1600 = v_sp1[7].append(v_sp2[7])
 v2000 = v_sp1[9].append(v_sp2[9])
+v = (v400, v800, v1200, v1600, v2000)
 
 # plot histogram
 fig = plt.figure(figsize=(15, 12))
-y400, bins = np.histogram(v400, bins=50, density=True)      # density: returns probability density
-y800 = np.histogram(v800, bins=50, density=True)      # density: returns probability density
-y1200 = np.histogram(v1200, bins=50, density=True)      # density: returns probability density
-y1600 = np.histogram(v1600, bins=50, density=True)      # density: returns probability density
-y2000 = np.histogram(v2000, bins=50, density=True)      # density: returns probability density
 
-binsize = (bins[1] - bins[0]) / 2
-binmean = bins[:-1] + binsize
+colors = ['purple', 'b', 'g', 'orange', 'r']
+labels = ['400', '800', '1200', '1600', '2000']
 
-plt.plot(binmean, y400, c='purple', lw=3, label='400')
-plt.plot(binmean, y800[0], c='b', lw=3, label='800')
-plt.plot(binmean, y1200[0], c='g', lw=3, label='1200')
-plt.plot(binmean, y1600[0], c='orange', lw=3, label='1600')
-plt.plot(binmean, y2000[0], c='r', lw=3, label='2000')
+for i in np.arange(5):
+    hist, bins = np.histogram(v[i], bins=50, density=True)
+    binsize = (bins[1] - bins[0]) / 2
+    binmean = bins[:-1] + binsize
+
+    plt.plot(binmean, hist, c=colors[i], lw=3, label=labels[i])
 
 plt.minorticks_on()
 plt.tick_params(axis='both', which='both', direction='in', labelsize=23)
